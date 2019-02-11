84abba44e6eed2a8a8d0631f1f9ec725c7ca4d30
Anton Biryukov <anton.biryukov@ucalgary.ca>
2019-02-10 23:28:43 -0700

    added tsfresh feature gen

--- Makefile
+++ Makefile
@@ -20,13 +20,8 @@ endif
 # COMMANDS                                                                      #
 #################################################################################
 
-## Install Python Dependencies
-requirements: test_environment
-	$(PYTHON_INTERPRETER) -m pip install -U pip setuptools wheel
-	$(PYTHON_INTERPRETER) -m pip install -r requirements.txt
-
 ## Make Dataset
-data: requirements
+data: 
 	$(PYTHON_INTERPRETER) src/data/make_dataset.py
 
 ## Delete all compiled Python files
@@ -34,47 +29,8 @@ clean:
 	find . -type f -name "*.py[co]" -delete
 	find . -type d -name "__pycache__" -delete
 
-## Lint using flake8
-lint:
-	flake8 src
 
-## Upload Data to S3
-sync_data_to_s3:
-ifeq (default,$(PROFILE))
-	aws s3 sync data/ s3://$(BUCKET)/data/
-else
-	aws s3 sync data/ s3://$(BUCKET)/data/ --profile $(PROFILE)
-endif
 
-## Download Data from S3
-sync_data_from_s3:
-ifeq (default,$(PROFILE))
-	aws s3 sync s3://$(BUCKET)/data/ data/
-else
-	aws s3 sync s3://$(BUCKET)/data/ data/ --profile $(PROFILE)
-endif
-
-## Set up python interpreter environment
-create_environment:
-ifeq (True,$(HAS_CONDA))
-		@echo ">>> Detected conda, creating conda environment."
-ifeq (3,$(findstring 3,$(PYTHON_INTERPRETER)))
-	conda create --name $(PROJECT_NAME) python=3
-else
-	conda create --name $(PROJECT_NAME) python=2.7
-endif
-		@echo ">>> New conda env created. Activate with:\nsource activate $(PROJECT_NAME)"
-else
-	$(PYTHON_INTERPRETER) -m pip install -q virtualenv virtualenvwrapper
-	@echo ">>> Installing virtualenvwrapper if not already intalled.\nMake sure the following lines are in shell startup file\n\
-	export WORKON_HOME=$$HOME/.virtualenvs\nexport PROJECT_HOME=$$HOME/Devel\nsource /usr/local/bin/virtualenvwrapper.sh\n"
-	@bash -c "source `which virtualenvwrapper.sh`;mkvirtualenv $(PROJECT_NAME) --python=$(PYTHON_INTERPRETER)"
-	@echo ">>> New virtualenv created. Activate with:\nworkon $(PROJECT_NAME)"
-endif
-
-## Test python environment is setup correctly
-test_environment:
-	$(PYTHON_INTERPRETER) test_environment.py
 
 #################################################################################
 # PROJECT RULES                                                                 #
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,22 +1,76 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
 import zipfile
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(root = None):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    import pandas as pd
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-    zip_ref = zipfile.ZipFile(input_filepath,'r')
-    zip_ref.extractall(output_filepath)
+    zip_ref = zipfile.ZipFile(f'{root}/data/raw/for_competition_release.zip','r')
+    zip_ref.extractall(f'{root}/data/interim/')
+    df_train = pd.read_csv(f'{root}/data/interim/train_data/all_train.csv',parse_dates=['date'])
+    data_dict = pd.read_json(f'{root}/data/interim/data_dictionary_v1.json')
+    df_test = pd.read_csv(f'{root}/data/interim/test_data/all_test.csv',parse_dates=['date'])
+    # Concatenate , index by test-train
+    cols = df_test.columns
+    # Fill NAs by propagating last available value
+    df_all = pd.concat([df_train[cols],df_test],keys = ['train','test']).fillna(method = 'ffill')
+    print(f'Train shape = {df_train.shape} , test shape = {df_test.shape}')
+    
+    
+    
+    df_all['hour'] = df_all['date'].dt.hour
+    df_all['dow'] = df_all['date'].dt.dayofweek
+    df_all['day'] = df_all['date'].dt.day / df_all['date'].dt.daysinmonth
+    df_all['month'] = df_all['date'].dt.month
+    df_all['weekofyear'] = df_all['date'].dt.weekofyear
+    df_all.to_csv(f'{root}/data/processed/train_test_raw.csv')
+    # Get tsfresh:
+    
+    df_tsfresh_feats = get_tsfresh_features(df_all.drop(['hour','dow','day','month','weekofyear'],axis = 1))
+    df_tsfresh_feats.to_csv(f'{root}/data/processed/train_test_tsfresh.csv')
+    
+    
+def get_tsfresh_features(df = None,max_timeshift = 3*10,n_jobs = 4,every_n_hour = 4):
+    from tsfresh.utilities.dataframe_functions import make_forecasting_frame
+    from tsfresh.utilities.dataframe_functions import impute
+    from tsfresh.feature_extraction import extract_features, MinimalFCParameters,EfficientFCParameters
+    d = EfficientFCParameters()
+    
+    del d['friedrich_coefficients']
+    del d['max_langevin_fixed_point']
+    del d['autocorrelation']
+    del d['agg_autocorrelation']
+    del d['cwt_coefficients']
+    del d['large_standard_deviation']
+    del d['symmetry_looking']
+    del d['partial_autocorrelation']
+    del d['index_mass_quantile']
+    del d['spkt_welch_density']
+    del d['change_quantiles']
+    del d['fft_aggregated']
+    del d['fft_coefficient']
+    del d['agg_linear_trend']
 
+    
+    df = df.fillna(method = 'ffill')
+    df['hour'] = df['date'].dt.hour
 
+    df_tsfresh = df.reset_index(drop=True).set_index('date')
+    df_tsfresh = df_tsfresh[(df_tsfresh.hour % every_n_hour) == 0].drop('hour',axis = 1)
+    dfs = {}
+    for c in df_tsfresh.columns:
+        print(f'Working on {c}...')
+        df_shift, y = make_forecasting_frame(df_tsfresh[c], kind="price", max_timeshift=max_timeshift, rolling_direction=1)
+        X = extract_features(df_shift, column_id="id", column_sort="time", column_value="value",
+                             impute_function=impute,show_warnings=False, default_fc_parameters=d,n_jobs=n_jobs)
+        dfs[c] = X
+    df_tsfresh_feats = pd.concat(dfs,keys = list(dfs.keys()))
+    return df_tsfresh_feats
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -24,7 +78,7 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
+    print(project_dir)
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    main()
+    main(root = project_dir)
