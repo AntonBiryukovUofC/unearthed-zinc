15c05718baeb2b8535bae9a5ee854b9be98f46b6
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2019-02-11 21:57:18 -0700

    set runtime env to use nvidia

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 
 docker build . -t unearthed_zinc
-#docker run -it -u $(id -u):$(id -g) -v /home/mjbirdge/Development/Python/unearthed-zinc:/unearthed-zinc unearthed_zinc
-docker run -it -v /home/mjbirdge/Development/Python/unearthed-zinc:/unearthed-zinc unearthed_zinc
+#docker run --runtime=nvidia -it -u $(id -u):$(id -g) -v /home/mjbirdge/Development/Python/unearthed-zinc:/unearthed-zinc unearthed_zinc
+docker run --runtime=nvidia -it -v /home/mjbirdge/Development/Python/unearthed-zinc:/unearthed-zinc unearthed_zinc
